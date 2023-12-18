633eaa0eb60486b0d322a3a1eca455087b7e6f25
Alexandre Fernandes Junior <alexandre.fernandes@tabiahealth.com>
2023-12-18 10:19:44 -0300

    useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+
+type Action = {
+  type: "increase" | "decrease" | "reset";
+  amount?: number;
+};
+
+const reducer = (state: number, action: Action): number => {
+  switch (action.type) {
+    case "increase":
+      state = state + (action.amount || 1);
+      break;
+    case "decrease":
+      state = state - (action.amount || 1);
+      break;
+    case "reset":
+      state = 0;
+  }
+  return state;
+};
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, 0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {count}
+
+      <button onClick={() => dispatch({ type: "increase", amount: 1 })}>
+        aumentar 1
+      </button>
+      <button onClick={() => dispatch({ type: "increase", amount: 10 })}>
+        aumentar 10
+      </button>
+      <button onClick={() => dispatch({ type: "decrease", amount: 1 })}>
+        diminuir 1
+      </button>
+      <button onClick={() => dispatch({ type: "decrease", amount: 10 })}>
+        diminuir 10
+      </button>
+      <button onClick={() => dispatch({ type: "reset" })}>reset</button>
     </div>
   );
 }
